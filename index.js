d37371782f3a1bfa4afdd3daefc2e8b3a0b4899b
njokojunior <njokojunior1@gmail.com>
2026-05-12 02:22:58 -0700

    Javascript class

---
+++ index.js
@@ -0,0 +1,11 @@
+const num1= 5;
+const num2= 10;
+
+// const squareSum = Math.pow(num1+num2,2)
+// console.log(squareSum)
+
+const difference = num1 - num2; 
+console.log(difference)
+
+console.log(Math.abs(difference))
+
---
+++ old.js
@@ -0,0 +1,28 @@
+// let student = 'Ivane'; 
+// const DOB = 2002;
+// let description = "Developeur web avec 5+ annees d'experience";
+
+// console.log("Qui est L'eleve?")
+// console.log("Nom de L'eleve: " + student)
+// keller
+
+let firstName = 'Aude';
+let lastName = "NSAJ";
+let city = "Douala";
+let country = 'Cameroun';
+let favoriteFood = 'Tripe sauter'; 
+const birthYear = 2003;
+let currentYear = 2026
+
+let age = currentYear - birthYear;
+let fullName = `${firstName} ${lastName}`;
+let userLocation = city + country;
+let ageIn5years = age + 5;
+let sentencedLength = fullName.length
+
+console.log(`Hello, my name is ${fullName}. I am ${age} years old and I live in ${city}, ${country}. My Favorite food is ${favoriteFood}. I was born in ${birthYear}. In 5 years, I will be ${ageIn5years} years old. My name has ${sentencedLength} characters`)
+
+// Template Literals   `safsavsagwavg safcsafsaf ${fullname}`
+
+
+
